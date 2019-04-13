d6a42e1b4534acc671bf2aabb8822b3c46fa3e0f
Paige Wiener <snowplushie@yahoo.com>
2019-04-13 22:50:11 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,7 @@ require "nokogiri"
      projects[title.to_sym] = {
         :image_link => project.css("div.project-thumbnail a img").attribute("scr").value,
         :description => project.css("p.bbcard_blurb").text,
-        :location => project.css("ul.project-meta li a span.location-name").text,
+        :location => project.css("ul.project-meta span.location-name").text,
         :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
       }
   end
