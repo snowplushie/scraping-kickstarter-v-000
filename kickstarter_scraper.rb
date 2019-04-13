fd95ecdf1339926e42c1f878d47032ead5e53e6c
Paige Wiener <snowplushie@yahoo.com>
2019-04-13 22:36:51 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,22 +1,21 @@
 # require libraries/modules here
 require "nokogiri"
 
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-  projects = {}
+  def create_project_hash
+    html = File.read('fixtures/kickstarter.html')
+    kickstarter = Nokogiri::HTML(html)
+    projects = {}
   
-  kickstarter.css("li.project.grid_4").each do |project|
-    title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {
-      image_link: project.css("div.project-thumbnail img").attribute('scr').value,
-      description : project.css("p.bbcard_blurb").text,
-      location: project.css("span.location-name").text,
-      percent_funded: project.css("li.funded strong").text.gsub('%', '').to_i
-    }
-end
+    kickstarter.css("li.project.grid_4").each do |project|
+     title = project.css("h2.bbcard_name strong a").text
+     projects[title.to_sym] = {
+        image_link: project.css("div.project-thumbnail img").attribute('scr').value,
+        description : project.css("p.bbcard_blurb").text,
+        location: project.css("span.location-name").text,
+        percent_funded: project.css("li.funded strong").text.gsub('%', '').to_i
+      }
+  end
 
-projects
-
+  projects
 
 end
