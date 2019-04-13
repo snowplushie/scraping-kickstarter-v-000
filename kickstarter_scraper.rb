2b9178dbef53de638e7ec82d110d2ed487d1da8a
Paige Wiener <snowplushie@yahoo.com>
2019-04-13 22:22:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,10 @@
 # require libraries/modules here
 require "nokogiri"
+require "pry"
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokigiri::HTML(html)
+  project = {}
+  kickstarter.css("li.project.grid_4").first
 end
