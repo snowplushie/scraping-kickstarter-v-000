d58f25819bb129317ab75c4245e0422e896a20c5
Paige Wiener <snowplushie@yahoo.com>
2019-04-13 22:39:50 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,10 +8,10 @@ require "nokogiri"
     kickstarter.css("li.project.grid_4").each do |project|
      title = project.css("h2.bbcard_name strong a").text
      projects[title] = {
-        :image_link => project.css("div.project-thumbnail img").attribute('scr').value,
+        :image_link => project.css("div.project-thumbnail img").attribute("scr").value,
         :description => project.css("p.bbcard_blurb").text,
         :location => project.css("span.location-name").text,
-        :percent_funded => project.css("li.funded strong").text.gsub('%', '').to_i
+        :percent_funded => project.css("li.funded strong").text.gsub("%", "").to_i
       }
   end
 
