3562d02389a1f97da8ad3018b658a83d122d08a8
Paige Wiener <snowplushie@yahoo.com>
2019-04-13 22:38:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,10 +5,9 @@ require "nokogiri"
     html = File.read('fixtures/kickstarter.html')
     kickstarter = Nokogiri::HTML(html)
     projects = {}
-  
     kickstarter.css("li.project.grid_4").each do |project|
      title = project.css("h2.bbcard_name strong a").text
-     projects[title.to_sym] = {
+     projects[title] = {
         image_link: project.css("div.project-thumbnail img").attribute('scr').value,
         description : project.css("p.bbcard_blurb").text,
         location: project.css("span.location-name").text,
